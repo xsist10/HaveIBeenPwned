a5bf7d9d847b3327346345b29166a829049dc57a
Thomas Shone <xsist10@gmail.com>
2014-07-06 10:39:08 +0200

    Updated arrays to old style and added PHP version constraint

--- src/HaveIBeenPwned.php
+++ src/HaveIBeenPwned.php
@@ -9,19 +9,19 @@ class HaveIBeenPwned
             throw new UnavailableException('allow_url_fopen disabled.');
         }
 
-        $context = stream_context_create([
-            'http' => [
+        $context = stream_context_create(array(
+            'http' => array(
                 'user_agent'          => 'xsist10-PHP-client'
-            ],
-            'ssl' => [
+            ),
+            'ssl' => array(
             	'method'              => 'GET',
                 'verify_peer'         => true,
                 'verify_depth'        => 5,
                 'cafile'              => __DIR__ . '/../cacert.pem',
                 'CN_match'            => 'www.haveibeenpwned.com',
                 'disable_compression' => true
-            ]
-        ]);
+            )
+        );
         
         $result = @file_get_contents($url, false, $context);
         if (empty($result))
